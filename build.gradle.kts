92e5f6f6040a5d82023af0bde4e74612157950c3
d01226091-wq <d01226091@gmail.com>
2026-09-16 00:50:39 +0300

    Update plugin versions in build.gradle.kts
    Updated Android and Kotlin plugin versions in build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.novapvp.client"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.novapvp.client"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }

407ff2461beef37a318de813b28edc01afcea3fa
d01226091-wq <d01226091@gmail.com>
2026-09-16 01:46:38 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,13 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-}
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
